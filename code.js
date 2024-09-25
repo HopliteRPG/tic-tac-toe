b7ae2cf01a118caeecea84697b167ff112ecd83f
Bill <kanellisvasilios@protonmail.com>
2024-09-25 16:21:57 -0400

    top row win condition working

--- code.js
+++ code.js
@@ -2,17 +2,32 @@ const gameboard = (function () {
   let gameboardArr = ["","","","","","","","",""];
   const printGameboardArr = () => console.log(gameboardArr);
 
+  const checkWinCondition = (array) => {
+    for(let i = 0; i < array.length-1; i++){
+      if(array[0] === "X" && array[1] === "X" && array[2] === "X"){
+        alert("X Player Victory");
+        break;
+      }
+      else if(array[0] === "O" && array[1] === "O" && array[2] === "O"){
+        alert("O Player Victory");
+        break;
+      }
+    }
+  }
+
   const addItem = (num,icon) => {
-
-
     const tempHolder = icon.toUpperCase();
 
     if(num <= 8 && gameboardArr[num] === ""){
       if(tempHolder === "X" || tempHolder === "O"){
         gameboardArr.splice(num,1,tempHolder);
+        checkWinCondition(gameboardArr);
+
       }
       else{
          alert("Input only X or O");
+         checkWinCondition(gameboardArr);
+
       }
     } 
   }
@@ -22,8 +37,9 @@ const gameboard = (function () {
 
 gameboard.printGameboardArr();
 
-gameboard.addItem(0,"x");
-gameboard.addItem(8,"o");
+gameboard.addItem(0,"o");
+gameboard.addItem(1,"o");
+gameboard.addItem(2,"o");
 
 
 
