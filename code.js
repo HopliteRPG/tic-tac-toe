403c04eec92a6dbf9d7605c52b4bc526267df1d5
Bill <kanellisvasilios@protonmail.com>
2024-10-02 22:03:07 -0400

    Horizontal win working

--- code.js
+++ code.js
@@ -2,29 +2,15 @@ const gameboard = (function () {
   let gameboardArr = ["","","","","","","","",""];
   const printGameboardArr = () => console.log(gameboardArr);
 
-  const checkWinCondition = (array) => {
-      const winConditionFunc = function(array,firstCell,secondCell,thirdCell){
-        if(array[firstCell] === "X" && array[secondCell] === "X" && array[thirdCell] === "X"){
-          alert("X Player Victory");
-        }
-        else if(array[firstCell] === "O" && array[secondCell] === "O" && array[thirdCell] === "O"){
-          alert("O Player Victory");
-        }
-      }
-      //Horizontal Win Conditions
-      if(array[0,1,2] !== ""){
-        winConditionFunc(array,0,1,2);
-      }
-      else if(array[3,4,5] !== ""){
-        winConditionFunc(array,3,4,5);
-      }
-      else if(array[6,7,8] !== ""){
-        winConditionFunc(array,6,7,8);
-      }
-    
-
-    
+  const isHorizontalWin = function(array,firstCell){
+    if(array[firstCell] === "X" && array[firstCell+1] === "X" && array[firstCell+2] === "X"){
+      alert("X Player Victory");
+    }
+    else if(array[firstCell] === "O" && array[firstCell+1] === "O" && array[firstCell+2] === "O"){
+      alert("O Player Victory");
+    }
   }
+
 
   const addItem = (num,icon) => {
     const tempHolder = icon.toUpperCase();
@@ -32,12 +18,16 @@ const gameboard = (function () {
     if(num <= 8 && gameboardArr[num] === ""){
       if(tempHolder === "X" || tempHolder === "O"){
         gameboardArr.splice(num,1,tempHolder);
-        checkWinCondition(gameboardArr);
+        isHorizontalWin(gameboardArr,0);
+        isHorizontalWin(gameboardArr,3);
+        isHorizontalWin(gameboardArr,6);
 
       }
       else{
          alert("Input only X or O");
-         checkWinCondition(gameboardArr);
+         isHorizontalWin(gameboardArr,0);
+         isHorizontalWin(gameboardArr,3);
+         isHorizontalWin(gameboardArr,6);
 
       }
     } 
@@ -51,10 +41,12 @@ gameboard.printGameboardArr();
 //If a row is full victory condition will not active find out why
 gameboard.addItem(0,"o");
 gameboard.addItem(1,"o");
-// gameboard.addItem(2,"x");
-// gameboard.addItem(3,"x");
+gameboard.addItem(2,"x");
+
+gameboard.addItem(3,"x");
 // gameboard.addItem(4,"x");
 // gameboard.addItem(5,"x");
+
 gameboard.addItem(6,"o");
 gameboard.addItem(7,"o");
 gameboard.addItem(8,"o");
